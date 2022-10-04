835ed4f5f42b41413aef27f4a5e9b8fe300e7ed5
Aaditry Choudhury <aaditrychoudhury@gmail.com>
2022-10-04 07:56:13 +0530

    Update StriversSheet:Leetcode:Set Matrix Zeroes.cpp

--- StriversSheet:Leetcode:Set Matrix Zeroes.cpp
+++ StriversSheet:Leetcode:Set Matrix Zeroes.cpp
@@ -1,51 +1,46 @@
-class Solution {
-public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        int x=1,y=1;
-        //checking the first row and col
-        for(int i=0;i<m;++i){
-            if(matrix[i][0] == 0){y=0;}
-        }
-        for(int j=0;j<n;++j){
-            if(matrix[0][j] == 0){x=0;}
-        }
-        //checking the rest of the matrix
-        for(int i =1;i<m;++i){
-            for(int j =1;j<n;++j){
-                if(matrix[i][j] == 0){
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
-                }
-            }
-        }
-        
-        //assigning zero to the rows and columns
-        for(int j=1;j<n;++j){
-            if(matrix[0][j] == 0){
-                for(int i =1;i<m;++i){
-                    matrix[i][j] =0;
-                }
-            }
-        }
-        
-        for(int i =1;i<m;++i){
-            if(matrix[i][0] == 0){
-                for(int j=1;j<n;++j){
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-        //if the first row and column intially had a zero then make the whole row and column as 0
-        if(x==0){
-            for(int j=0;j<n;++j){matrix[0][j] = 0;}
-        }
-        if(y==0){
-            for(int i=0;i<m;++i){matrix[i][0]=0;}
-        }
-        
-        
-        
+#include <bits/stdc++.h> 
+#include <bits/stdc++.h> 
+void setZeros(vector<vector<int>> &matrix)
+{
+	// Write your code here.
+    int m = matrix.size();
+    int n = matrix[0].size();
+    int x=0,y=0;
+    //checking for the first row and column
+    for(int i =0;i<m;++i){
+        if(matrix[i][0] == 0) y=1;
     }
-};
+    for(int j=0;j<n;++j){
+        if(matrix[0][j] == 0) x=1;
+    }
+    //for the rest of the array m-1Xn-1
+    for(int i=1;i<m;++i){
+        for(int j=1;j<n;++j){
+            if(matrix[i][j] ==0){
+                matrix[i][0] = 0;
+                matrix[0][j]=0;
+            }
+        }
+    }
+    //checking in the dummmy array if zero then change in m-1Xn-1 //matrix
+    for(int i=1;i<m;++i){
+        if(matrix[i][0] == 0){
+            for(int j=1;j<n;++j){
+                matrix[i][j] =0;
+            }
+        }
+    }
+    for(int j=1;j<n;++j){
+        if(matrix[0][j] == 0){
+            for(int i=1;i<m;++i){
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    //for the dummy rows
+    if(y == 1){
+        for(int i=0;i<m;++i) matrix[i][0] =0;
+    }
+    if(x == 1){for(int j=0;j<n;++j) matrix[0][j] =0;}
+    
+}
